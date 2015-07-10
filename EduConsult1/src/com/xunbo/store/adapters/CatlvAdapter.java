e040cabcf07365445a99398e64f52ffc1c1c18cf
quzongren <905290841@qq.com>
2015-07-10 16:31:18 +0800

    更新

--- EduConsult1/src/com/xunbo/store/adapters/CatlvAdapter.java
+++ EduConsult1/src/com/xunbo/store/adapters/CatlvAdapter.java
@@ -7,11 +7,14 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.xunbo.store.R;
+import com.xunbo.store.activitys.CatDetaileActivity.Cat;
 import com.xunbo.store.beans.FenleiBean;
 import com.xunbo.store.beans.ListFenleiBean;
 import com.xunbo.store.myviews.MyListview;
@@ -27,13 +30,15 @@ public class CatlvAdapter extends BaseExpandableListAdapter {
 	private Item item;
 	private int index,width,height,w;
 	private CatHomelvAdapter catHomelvAdapter;
+	private Cat cat;
 
 	public CatlvAdapter(Context context,ListFenleiBean list, int index, LayoutInflater inflater2,
-			int groupPosition, CatHomelvAdapter catHomelvAdapter, int we){
+			int groupPosition, CatHomelvAdapter catHomelvAdapter, int we, Cat cat){
 		this.context = context;
 		this.catHomelvAdapter = catHomelvAdapter;
 		this.list = list;
 		this.index = index;
+		this.cat = cat;
 		this.w = we;
 		group = list.getList().get(index).getChild();
 		inflater = inflater2;//LayoutInflater.from(context);
@@ -85,11 +90,15 @@ public class CatlvAdapter extends BaseExpandableListAdapter {
 		Catlv3Adapter ap = new Catlv3Adapter(context, child);
 		halderItem.lv.setAdapter(ap);
 		halderItem.lv.setFocusable(false);
-//		LinearLayout.LayoutParams params = //new LayoutParams(LayoutParams.FILL_PARENT, 200);
-//				new LayoutParams(
-//						Util.getWidth((Activity)context), 5);//UITools.dip2px((Activity)context, 104));
-//		halderItem.lv.setLayoutParams(params);
-		///////////////////////////////////////////////////////////////////////
+		halderItem.lv.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				String catid = ""+child.get(arg2).getCatid();
+				cat.sreach(catid);
+			}
+		});
 		
 		
 		
